9c92fe1f81fd1b4c1eb587e7ea8361cd876eaca1
mario <mariolopezzbueno@gmail.com>
2024-05-21 15:13:02 +0200

    updated superset image version to 4.0.0

--- tests/image/build.sh
+++ tests/image/build.sh
@@ -1,3 +1,3 @@
-podman build . --tag docker.io/kubernetesbigdataeg/superset:2.1.0-1
+podman build . --tag docker.io/kubernetesbigdataeg/superset:4.0.0-1
 podman login docker.io -u kubernetesbigdataeg
-podman push docker.io/kubernetesbigdataeg/superset:2.1.0-1
+podman push docker.io/kubernetesbigdataeg/superset:4.0.0-1
